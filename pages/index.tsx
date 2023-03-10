526a4138b0c2ce4f2ce75271b3117d5961568062
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-03-10 15:44:14 +0200

    Feat : add style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 import Link from 'next/link'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
